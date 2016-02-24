1a5de0705e14f53e6f74384c9db890621e75982d
iceStone <ice@micua.com>
2016-02-24 15:29:30 +0800

    20160224

---
+++ sign/sign.cmd
@@ -0,0 +1,16 @@
+@REM @Author: iceStone
+@REM @Date:   2016-02-20 15:28:51
+@REM @Last Modified by:   iceStone
+@REM Modified time: 2016-02-20 15:30:00
+
+.\rcedit "Electron.exe" --set-icon "logo.ico"
+.\rcedit "Electron.exe" --set-version-string "ProductVersion" "2.0.0"
+.\rcedit "Electron.exe" --set-version-string "ProductName" "Itcast Evaluation"
+.\rcedit "Electron.exe" --set-version-string "OriginalFilename" "evaluation.exe"
+.\rcedit "Electron.exe" --set-version-string "InternalName" "evaluation.exe"
+.\rcedit "Electron.exe" --set-version-string "LegalCopyright" "Copyright (C) 2016 Itcast, Inc. All rights reserved."
+.\rcedit "Electron.exe" --set-version-string "FileDescription" "传智播客教学测评系统"
+.\rcedit "Electron.exe" --set-version-string "CompanyName" "Itcast, Inc."
+.\rcedit "Electron.exe" --set-product-version "2.0.0"
+.\rcedit "Electron.exe" --set-file-version "2.0.0"
+.\rcedit "Electron.exe" --set-version-string "Comments" "传智播客教学测评系统"
